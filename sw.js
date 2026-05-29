2cf12ba6af9e431d959ecf89124e2e60b53cc73b
ContactRelayops <contactrelayops@gmail.com>
2026-05-28 21:38:28 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE = 'relayops-v1';
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
